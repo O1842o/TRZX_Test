b102cecee3f4691b1b2de778d19d413deff8ab59
zhangjw@mmwipo.com <zhangjw@mmwipo.com>
2017-02-17 13:48:16 +0800

    Initial Commit

---
+++ TRZX_Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TRZX_Test
+//
+//  Created by 张江威 on 2017/2/17.
+//  Copyright © 2017年 张江威. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TRZX_Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TRZX_Test
+//
+//  Created by 张江威 on 2017/2/17.
+//  Copyright © 2017年 张江威. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
